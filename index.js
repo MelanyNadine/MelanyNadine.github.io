aaec7e6d8a54e2a5558772d027e9f52f24b4a3c8
Dev Nadine <devian@localhost.localdomain>
2023-08-08 23:47:22 -0500

    updates

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const myArtItems = [
 const articles = {
 	"cosmological-constant": "https://melanynadine.github.io/m_articles/cosmological_constant.html",
   "coronavirus": "https://melanynadine.github.io/m_articles/coronavirus_outbreak_pdf.html",
-  "best-apps-to-learn-chinese": "https://melanynadine.github.io/m_articles/best_apps_to_learn_mandarin_chinese.html"
 }
 
 
